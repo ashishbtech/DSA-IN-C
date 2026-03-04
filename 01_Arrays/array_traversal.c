beda5f7b76cc2b6586e66d445bcfa1dc346cd446
ashishbtech <ashishbtech.cs@gmail.com>
2026-03-04 15:13:00 +0530

    Day 1 revising Array fundamentals before moving further

---
+++ 01_Arrays/array_traversal.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int arr[]={10,20,30,40,50};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    printf("Array elements: \n");
+    for(int i=0;i<n; i++){
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
+
+
+//TIME COMPLEXITY: O(n)
+//SPACE COMPLEXITY: O(1)
