eee5f4948f2aba68e7bef36de3092f32854008d0
ashishbtech <ashishbtech.cs@gmail.com>
2026-03-11 22:10:29 +0530

    Recursion: Implemented Fibonacci sequence using recursion

---
+++ 04_Recursion/fibonacci_recursion.c
@@ -0,0 +1,31 @@
+// Problem
+// Print the Nth Fibonacci number using recursion.
+#include <stdio.h>
+int fibonacci(int n)
+{
+    
+    if (n == 0)
+        return 0;
+
+    if (n == 1)
+        return 1;
+
+    
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main()
+{
+    int n = 6;
+
+    int result = fibonacci(n);
+
+    printf("Fibonacci = %d", result);
+
+    return 0;
+}
+
+/*
+Time Complexity: O(2^n)
+Space Complexity: O(n)
+*/
